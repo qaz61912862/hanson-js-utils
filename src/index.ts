7b087c6c2e9cba50ae44d9fa8095857096c72072
“laixin” <774612160@qq.com>
2021-04-06 18:55:40 +0800

    docs: reset

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 export * from './object';
 export * from './date';
+// let utils: any = {};
+
+// const modules = require.context('./modules/', true, /.ts$/);
+
+// modules.keys().forEach(modulesKey => {
+//   utils['test'] = modules(modulesKey);
+// });
+
+// module.exports = utils;
---
+++ src/modules/index.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-04-06 18:25:23
+ * @LastEditTime: 2021-04-06 18:29:07
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \hanson-js-utils\src\modules\index.ts
+ */
+module.exports = {
+  get: function() {
+    return 'get'
+  }
+}
