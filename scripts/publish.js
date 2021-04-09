4de59c08ef81537fe14562d3dcc8831e106a4938
“laixin” <774612160@qq.com>
2021-04-09 11:10:00 +0800

    fix: 增加备注

--- scripts/publish.js
+++ scripts/publish.js
@@ -16,7 +16,7 @@ let newVersion = defaultVersion;
  
 // // 从命令行参数中取版本号输入 npm run buildAndPublish -v 9.9.9
 program
-  .option('-v, --versions <type>', 'Add release version number');
+  .option('-v, --versions <type>', 'Add release version number', defaultVersion);
  
 program.parse(process.argv);
 
@@ -25,18 +25,18 @@ const options = program.opts();
 if (options.versions) {
   newVersion = options.versions;
 }
+// console.log(newVersion);
 
 function publish() {
   shelljs.sed('-i', '"name": "hanson"', '"name": "@hanson/hanson-js-utils"', targetFile); // 修改包名
   shelljs.sed('-i', `"version": "${currentVersion}"`, `"version": "${newVersion}"`, targetFile); // 修改版本号
   shelljs.cd('dist');
-  // 同步修改dist目录下的package.json
   var cbDataPackage = JSON.parse(fs.readFileSync('../package.json'))
   cbDataPackage.version = newVersion
   // 重写dist下的package.json
   fs.writeFile('../package.json', JSON.stringify(cbDataPackage), function(err) {
     if (err) console.error(err)
-    console.log('----------------------修改package.json文件完毕，version修改为：', cbDataPackage.version)
+    // console.log('----------------------修改package.json文件完毕，version修改为：', cbDataPackage.version)
   })
   // 执行命令行
   shelljs.exec('npm publish'); // 发布
